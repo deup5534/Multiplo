4b32f2779fd3c5e0ee42bda46b67383c5e603bdb
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-09-30 22:22:16 -0300

    lindo igual você

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿Console.WriteLine("----MÚLTIPLO----");
 int n1, n2, resto;
 
-Console.Write("digita um número legal aí:");
+Console.Write("\ndigita um número legal aí:");
 n1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("digita um outro número legal aí:");
+Console.Write("\ndigita um outro número legal aí:");
 n2 = Convert.ToInt32(Console.ReadLine());
 
 resto = n1 % n2;
 
 if (resto == 0)
 {
-    Console.WriteLine($"{n1} é múltiplo de {n2}");
+    Console.WriteLine($"\n{n1} é múltiplo de {n2}");
 }
 else
 {
-    Console.WriteLine($"{n1} não é múltiplo de {n2}");
+    Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
 }
 
 Console.ForegroundColor = ConsoleColor.Green;
